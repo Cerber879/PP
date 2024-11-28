4ed8ae3d6b4e09fd3df3004acfc4189180bfa3e9
Cerber879 <nik.afansev789@mail.ru>
2024-11-28 13:22:17 +0300

    add lab5

---
+++ lab5/sync.bat
@@ -0,0 +1,2 @@
+start "" "C:\Users\User\source\repos\PP\lab5\x64\Debug\lab5.exe"
+start "" "C:\Users\User\source\repos\PP\lab5\x64\Debug\lab5.exe"
---
+++ lab5_2/sync.bat
@@ -0,0 +1,2 @@
+start "" "C:\Users\User\source\repos\PP\lab5_2\x64\Debug\lab5_2.exe"
+start "" "C:\Users\User\source\repos\PP\lab5_2\x64\Debug\lab5_2.exe"
